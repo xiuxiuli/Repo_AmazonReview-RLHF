74a6d498b097a26738721ba15e116d34df302c92
Xiu <>
2025-10-20 14:47:28 -0700

    Create colab_setup.py

---
+++ utils/colab_setup.py
@@ -0,0 +1,13 @@
+# from google.colab import drive
+# drive.mount('/content/drive')
+
+# !git clone https://github.com/xiuxiuli/Repo_AmazonReview-RLHF.git /content/drive/MyDrive/amazon_review_RLHF
+
+# %cd /content/drive/MyDrive/amazon_review_RLHF
+
+# !git pull
+
+# !pip install -r requirements.txt
+
+# !python ./data/download.py
+
